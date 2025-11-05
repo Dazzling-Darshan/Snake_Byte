25ce697afe1d40eeec1117f62c90430be158c33f
mayank-jayswal <jayswalmayank05@gmail.com>
2025-11-05 12:35:10 +0530

    Update game.h

--- game.h
+++ game.h
@@ -36,5 +36,52 @@ public:
     int getLength() const;
 };
 
-#endif
+// ==========================
+// Commit 2 additions below
+// ==========================
 
+class Game {
+private:
+    Snake snake;
+    pair<int, int> food;
+    pair<int, int> specialFood;
+    int score;
+    bool gameOver;
+    bool quit;
+    Screen screen;
+    int foodEaten;
+    bool specialFoodActive;
+    chrono::steady_clock::time_point specialFoodSpawnTime;
+    const int SPECIAL_FOOD_DURATION = 10;
+    pair<int, int> crashPosition;
+    bool wallCrash;
+
+    // Obstacle handling
+    const int OBSTACLE_DURATION = 8;
+    const int OBSTACLE_COUNT = 5;
+    vector<pair<int, int>> obstacles;
+    chrono::steady_clock::time_point obstacleSpawnTime;
+    bool obstaclesActive;
+
+    // Speed control
+    int baseSpeed = 200;
+    int minSpeed = 75;
+    int speedDecrement = 9;
+
+    // Helper methods (to be implemented later)
+    bool isObstacle(int x, int y) const;
+    void spawnFood();
+    void spawnSpecialFood();
+    void spawnObstacles();
+    bool checkCollision();
+    void resetGame();
+    void updateSpecialFood();
+    void updateObstacles();
+    void saveHighScore();
+    int loadHighScore();
+
+public:
+    Game();  // Constructor
+};
+
+#endif
