ead93b1abbfc399a9b384568ec5a174d93bf64d8
mayank-jayswal <jayswalmayank05@gmail.com>
2025-11-05 12:26:05 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include "game.h"   // preparing for InputHandler and Game classes
+
 using namespace std;
 
 int main() {
@@ -9,6 +11,30 @@ int main() {
     cout << "Starting in 2 seconds..." << endl;
     
     this_thread::sleep_for(chrono::seconds(2));
-    
+
+    // --- Clear screen properly at start ---
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+
+    // --- Enable raw input ---
+    InputHandler::enableRawInput();
+
+    cout << "Raw input enabled successfully!" << endl;
+    cout << "Press any key to exit setup test..." << endl;
+
+    // Wait for a single keypress to test input
+    while (!InputHandler::isKeyPressed()) {
+        this_thread::sleep_for(chrono::milliseconds(100));
+    }
+
+    InputHandler::getChar();  // consume key press
+
+    // --- Disable raw input before exit ---
+    InputHandler::disableRawInput();
+
+    cout << "Exiting setup..." << endl;
     return 0;
 }
