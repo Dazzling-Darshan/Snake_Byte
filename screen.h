58a7c76958d86cf59ae815242fadf82337fa1460
Darshan Prajapati <167950768+Dazzling-Darshan@users.noreply.github.com>
2025-11-05 11:13:40 +0530

    screen.h updated
    Added console dimensions and default emojis as extern variables.

--- screen.h
+++ screen.h
@@ -14,3 +14,17 @@
 #endif
 
 using namespace std;
+
+// Console dimensions
+const int WIDTH = 40;
+const int HEIGHT = 20;
+
+// Default emojis (fallback)
+extern string SNAKE_HEAD;
+extern string SNAKE_HEAD_DEAD;
+extern string SNAKE_BODY;
+extern string SNAKE_BODY_DEAD;
+extern string FOOD_EMOJI;
+extern string SPECIAL_FOOD_EMOJI;
+extern string WALL;
+extern string EMPTY_SPACE;
